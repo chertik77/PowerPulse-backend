d560227c3507e0327b60b958e2648046fae7be5e
chertik77 <babychdenya@gmail.com>
2024-04-29 18:52:02 +0100

    chore: update dependecies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [ConfigModule.forRoot({ isGlobal: true })]
 })
 export class AppModule {}
