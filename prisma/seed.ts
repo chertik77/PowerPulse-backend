01884213a81dedc04c12555525e980e6e25d68e7
chertik77 <denysbabych.dev@gmail.com>
2026-05-02 14:10:03 +0100

    refactor(seed): remove seed history tracking

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -13,51 +13,25 @@ const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
 const prisma = new PrismaClient({ adapter })
 
-const hasSeedRun = async (id: string) => {
-  const seedHasRun = await prisma.seedHistory.findUnique({
-    where: { id }
-  })
-
-  return !!seedHasRun
-}
-
-const markSeedAsRun = async (id: string) => {
-  await prisma.seedHistory.create({
-    data: { id }
-  })
-}
-
 const productsData: Prisma.ProductCreateInput[] = products
 const exercisesData: Prisma.ExerciseCreateInput[] = exercises
 const exerciseFiltersData: Prisma.ExerciseFilterCreateInput[] = exerciseFilters
 
 const main = async () => {
-  if (!(await hasSeedRun('001-products'))) {
-    await prisma.product.createMany({
-      data: productsData,
-      skipDuplicates: true
-    })
+  await prisma.product.createMany({
+    data: productsData,
+    skipDuplicates: true
+  })
 
-    await markSeedAsRun('001-products')
-  }
+  await prisma.exerciseFilter.createMany({
+    data: exerciseFiltersData,
+    skipDuplicates: true
+  })
 
-  if (!(await hasSeedRun('002-exercise-filters'))) {
-    await prisma.exerciseFilter.createMany({
-      data: exerciseFiltersData,
-      skipDuplicates: true
-    })
-
-    await markSeedAsRun('002-exercise-filters')
-  }
-
-  if (!(await hasSeedRun('003-exercises'))) {
-    await prisma.exercise.createMany({
-      data: exercisesData,
-      skipDuplicates: true
-    })
-
-    await markSeedAsRun('003-exercises')
-  }
+  await prisma.exercise.createMany({
+    data: exercisesData,
+    skipDuplicates: true
+  })
 }
 
 main()
